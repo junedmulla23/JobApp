c2cd4a98709b10d47684cb0dcdd335c77090d9d3
Juned Mulla <junedmulla2869@gmail.com>
2025-11-26 14:03:35 +0530

    Add files via upload

---
+++ JobAppManagement/src/main/java/com/JobApp/JobAppManagement/JobAppManagementApplication.java
@@ -0,0 +1,18 @@
+package com.JobApp.JobAppManagement;
+
+import com.JobApp.JobAppManagement.Model.JobPost;
+import com.JobApp.JobAppManagement.Repository.Jobrepo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobAppManagementApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JobAppManagementApplication.class, args);
+
+
+	}
+	}
+
